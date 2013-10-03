728b33aa331f1463d263a54745b0d7299db47afa
adwitz <adam.witzel@gmail.com>
2013-10-03 12:38:30 -0700

    progress on N queens

--- src/solvers.js
+++ src/solvers.js
@@ -91,17 +91,120 @@ window.makeArray = function(chessObj){
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n){
-  var solution = undefined; //fixme
-
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  var aboard = new Board({n:n});
+  aboard = aboard.rows();
+  var breadcrumbs;
+  var solutions = [];
+  var traverse = function(board, rowNum, breadcrumbs){
+    if (rowNum === n){
+      if (board.counter()){
+        solutions.push(board);
+      }
+      return;
+    }
+    for (var col = 0; col < n; col++){
+      if (!(breadcrumbs["col" + col])){
+        var newBoard = copy(board);
+        newBoard[rowNum][col] = 1;
+        newBoard.breadcrumbs = breadcrumbs.clone();
+        newBoard.breadcrumbs["col" + col] = true;
+        if (!(newBoard.hasAnyMajorDiagonalConflicts() || newBoard.hasAnyMinorDiagonalConflicts())){
+          traverse(newBoard, rowNum+1, newBoard.breadcrumbs);
+        }
+      }
+    }
+  };
+  traverse(aboard, 0, {});
+  if ( solutions[0] ){
+    return solutions[0];
+  } else {
+    return new Board({n:n}).rows();
+  }
 };
+
+Array.prototype.counter = function(){
+  var results = 0;
+  for(var i=0; i<this.length; i++){
+    for (var j=0; j<this[i].length; j++){
+      this[i][j] && results++;
+    }
+  }
+  if (results === this.length){ return true; }
+  return false;
+};
+
+Array.prototype.hasMajorDiagonalConflictAt = function(majorDiagonalColumnIndexAtFirstRow){
+      var count = 0;
+      var colIn = majorDiagonalColumnIndexAtFirstRow;
+      var chessboard = this;
+      for (var row = 0; row < chessboard.length; row++){
+        chessboard[row][colIn] && count++;
+        colIn++;
+        if (count > 1) { return true; }
+      }
+      return false; // fixme
+    };
+
+    // test if any major diagonals on this board contain conflicts
+Array.prototype.hasAnyMajorDiagonalConflicts = function(){
+      var chessboard = this;
+      var length = chessboard.length;
+      for ( var i = 0; i < length; i++ ) {
+        if (this.hasMajorDiagonalConflictAt(i)) { return true; }
+      }
+      return false; // fixme
+    };
+
+Array.prototype.hasMinorDiagonalConflictAt = function(minorDiagonalColumnIndexAtFirstRow){
+      var chessboard = this;
+      var colIn = minorDiagonalColumnIndexAtFirstRow;
+      var length = chessboard.length;
+      var count = 0;
+      for ( var row = 0; row < length; row++ ){
+        chessboard[row][colIn] && count++;
+        colIn--;
+        if (count > 1) { return true; }
+      }
+      return false; // fixme
+    };
+
+    // test if any minor diagonals on this board contain conflicts
+Array.prototype.hasAnyMinorDiagonalConflicts = function(){
+      var chessboard = this;
+      var length = chessboard.length;
+      for ( var i=length-1 ; i > -1; i--){
+        if(this.hasMinorDiagonalConflictAt(i)) { return true; }
+      }
+      return false; // fixme
+    };
 
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n){
-  var solutionCount = undefined; //fixme
-
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+  var aboard = new Board({n:n});
+  aboard = aboard.rows();
+  var breadcrumbs;
+  var solutions = [];
+  var traverse = function(board, rowNum, breadcrumbs){
+    if (rowNum === n){
+      if (board.counter()){
+        debugger;
+        solutions.push(board);
+      }
+      return;
+    }
+    for (var col = 0; col < n; col++){
+      if (!(breadcrumbs["col" + col])){
+        var newBoard = copy(board);
+        newBoard[rowNum][col] = 1;
+        newBoard.breadcrumbs = breadcrumbs.clone();
+        newBoard.breadcrumbs["col" + col] = true;
+        if (!(newBoard.hasAnyMajorDiagonalConflicts() || newBoard.hasAnyMinorDiagonalConflicts())){
+          traverse(newBoard, rowNum+1, newBoard.breadcrumbs);
+        }
+      }
+    }
+  };
+  traverse(aboard, 0, {});
+  return solutions.length;
 };
