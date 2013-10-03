10e55a2fb742608ade85eed3f6c28f8d00f99ccc
adwitz <adam.witzel@gmail.com>
2013-10-02 21:14:09 -0700

    lack of progress but code written

--- src/solvers.js
+++ src/solvers.js
@@ -18,33 +18,38 @@ window.findNRooksSolution = function(n){
   var storage = [];  //all possible chessboards;
   var solutions = [];
   var rookSearch = function(chessboard, row){
-    for (var col = 0; col < n; col++){  //each time we call rookSearch checkout every column
-      var newChessboard = new Board(chessboard.attributes);   //make a new board that has the same values as the previous board
-      if (newChessboard.attributes[row] !== undefined){
-        newChessboard.attributes[row][col] = 1;  //for the current value of column, set that row's column value to be 1
+    if (row === n){
+        solutions.push(chessboard);
+    } else {
+      for (var col = 0; col < n; col++){
+        var newChessboard = new Board(makeArray(chessboard));
+        debugger;
+        newChessboard.attributes[row][col] = 1;
+        var blah = newChessboard.hasAnyColConflicts();
+        var bleh = newChessboard.hasAnyRowConflicts();
+        rookSearch(newChessboard, row+1);
       }
-      row = row + 1;  //increase row by one to go to the next row;
-      if (row === n){ storage.push(newChessboard); }  //if there are no more next rows add the current chessboard to storage
-      else { rookSearch(newChessboard, row); }  //otherwise do the same algorithm to the next row
     }
   };
   rookSearch(new Board({n:n}), 0);
   debugger;
-  for (var i=0; i < storage.length; i++){
-      if (!(storage[i].hasAnyColConflicts() || storage[i].hasAnyRowConflicts())){
-        solutions.push(storage[i]);
-      }
-    }
   return solutions[0].attributes;
 };
-
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n){
   // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return window.solutionNumbers[n];
+  return ;
 };
 
+window.makeArray = function(chessObj){
+  var array = [];
+  obj = chessObj.attributes;
+  for ( var i = 0; i < obj.n; i ++){
+    array.push(obj[i]);
+  }
+  return array;
+}
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
